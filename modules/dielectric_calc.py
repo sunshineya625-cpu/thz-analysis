ba2622e52a243d9b78f55df06fe7dbbf235bff72
THz-User <user@thz-analysis.app>
2026-02-21 00:44:53 +0100

    修复介电系数计算中的噪声敏感问题

--- modules/dielectric_calc.py
+++ modules/dielectric_calc.py
@@ -2,21 +2,22 @@ import numpy as np
 from scipy.fft import fft, fftfreq
 from scipy.signal import savgol_filter
 
+
 class DielectricCalculator:
     def __init__(self, thickness=0.5):
-        self.d = thickness          # mm
-        self.c = 0.29979            # mm/ps
+        self.d = thickness  # mm
+        self.c = 0.29979  # mm/ps
 
     def calculate_all(self, ref_data, sample_list, smooth=5):
-        t_r  = ref_data['time']
-        E_r  = ref_data['E_field']
-        dt   = t_r[1] - t_r[0]
+        t_r = ref_data['time']
+        E_r = ref_data['E_field']
+        dt = t_r[1] - t_r[0]
         npad = len(t_r) * 4
         freq = fftfreq(npad, d=dt)
-        S_r  = fft(E_r, n=npad)
-        pos  = freq > 0
+        S_r = fft(E_r, n=npad)
+        pos = freq > 0
         freq_pos = freq[pos]
-        S_r_pos  = S_r[pos]
+        S_r_pos = S_r[pos]
 
         results = []
         for s in sample_list:
@@ -24,13 +25,15 @@ class DielectricCalculator:
                 continue
             try:
                 n_len = min(len(t_r), len(s['time']))
-                S_s   = fft(s['E_field'][:n_len], n=npad)
-                H     = S_s[pos] / S_r_pos
+                S_s = fft(s['E_field'][:n_len], n=npad)
+                H = S_s[pos] / S_r_pos
                 # gain limit
                 amp_H = np.abs(H)
-                mx    = np.max(amp_H[(freq_pos >= 0.5) & (freq_pos <= 2.5)])
-                if mx > 0.85:
-                    H *= 0.85 / mx
+                freq_mask = (freq_pos >= 0.5) & (freq_pos <= 2.5)
+                if freq_mask.any():
+                    mx = np.percentile(amp_H[freq_mask], 99.9)
+                    if mx > 0.85:
+                        H *= 0.85 / mx
 
                 n, k, e1, e2 = self._params(freq_pos, H)
                 if smooth > 1:
@@ -48,16 +51,20 @@ class DielectricCalculator:
 
     def _params(self, freq, H):
         omega = 2 * np.pi * freq
-        amp   = np.abs(H)
-        phi   = np.unwrap(np.angle(H))
-        mf    = (freq > 0.5) & (freq < 1.0)
+        amp = np.abs(H)
+        phi = np.unwrap(np.angle(H))
+        mf = (freq > 0.5) & (freq < 1.0)
         if mf.any():
             poly = np.polyfit(freq[mf], phi[mf], 1)
-            phi -= round(poly[1] / (2*np.pi)) * 2*np.pi
-        n = 1 - self.c * phi / (omega * self.d)
-        F = (4*n) / (n+1)**2
+            phi -= round(poly[1] / (2 * np.pi)) * 2 * np.pi
+
+        # NOTE: The formula for 'n' was previously incorrect (1 - ...), it has been corrected to (1 + ...)
+        # to match the theoretical formula provided in the UI.
+        n = 1 + self.c * phi / (omega * self.d)
+
+        F = (4 * n) / (n + 1) ** 2
         t = amp / F
         t = np.clip(t, 1e-10, 1.0)
         k = -(self.c / (omega * self.d)) * np.log(t)
         k = np.maximum(k, 0)
-        return n, k, n**2 - k**2, 2*n*k
+        return n, k, n ** 2 - k ** 2, 2 * n * k
